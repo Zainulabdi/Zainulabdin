055ff93124d877a8f6d09b2921c1f864adcf2480
Shahzaib Shah <97381798+Zainulabdi@users.noreply.github.com>
2022-05-07 15:40:03 +0500

    main page

--- tasbeeh/lib/createTashbeh.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-import 'createTashbeh.dart';
-import 'main.dart';
--- tasbeeh/lib/main.dart
+++ tasbeeh/lib/main.dart
@@ -1,118 +1,34 @@
 import 'package:flutter/material.dart';
-import 'createTashbeh.dart';
-import 'countertasbeh.dart';
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+void main() => runApp(labtask());
+
+
+class labtask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tasbeeh App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Tasbeeh app for Muslims'),
+      home: InputPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
+class InputPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _InputPageState createState() => _InputPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Tasbeeh'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => createTashbeh()));
-                    },
-                    child: Container(
-                      child: Center(
-                        child: Text(
-                          'Create Tasbeeh',
-                        ),
-                      ),
-                    ),
-                  )
-                ]
-            ),
-            ),
-            Expanded(child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => countertasbeh()));
-                    },
-                    child: Container(
-                      child: Center(
-                        child: Text(
-                          'Counter Tasbeeh',
-                        ),
-                      ),
-                    ),
-                  )
-                ]
-            ),
-            ),
-            Expanded(child: Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => createTashbeh()));
-                    },
-                    child: Container(
-                      child: Center(
-                        child: Text(
-                          'View All Tasbeeh',
-                        ),
-                      ),
-                    ),
-                  )
-                ]
-            ),
-            ),
-          ],
-        ),
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
-
-  createTashbeh() {}
-
-  countertasbeh() {}
 }
