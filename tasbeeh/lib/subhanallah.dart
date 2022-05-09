4de37347798891f29fc17d001ecd8f3ada969e6c
Shahzaib Shah <97381798+Zainulabdi@users.noreply.github.com>
2022-05-09 17:11:59 +0500

    Counter Tasbeeh  in (Subhanallah ) screen

--- tasbeeh/lib/subhanallah.dart
+++ tasbeeh/lib/subhanallah.dart
@@ -1,16 +1,85 @@
 import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const subhanallah());
+}
+
 class subhanallah extends StatelessWidget {
-  const subhanallah({key}) : super(key: key);
+  const subhanallah({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Subhan Allah',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Subhanallah'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, required string, this.title }) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class required {
+  @override
+  String toString() {
+    // TODO: implement toString
+    return super.toString();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SUBHAN ALLAH'),
+
+        title: Text(widget.title),
       ),
       body: Center(
 
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'TOTAL COUNTS OF THE TASBEEH:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
