4bbbaa53940c59add2959085b47f748fb280eaab
Shahzaib Shah <97381798+Zainulabdi@users.noreply.github.com>
2022-07-15 19:17:25 +0500

    Fee managment system

--- fee_manigment_apps/android/app/src/main/kotlin/com/example/fee_manigment_apps/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fee_manigment_apps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- fee_manigment_apps/lib/main.dart
+++ fee_manigment_apps/lib/main.dart
@@ -1,61 +1,28 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'splashscreen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  _MyAppState createState() => _MyAppState();
-}
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+  }
 
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Fee Manigment '),
-          ),
-          body: Center(
-              child: Column(children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(25),
-                  child: FlatButton(
-                    child: Text(
-                      'Manager',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    color: Colors.cyan,
-                    textColor: Colors.black,
-                    onPressed: () {},
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(25),
-                  child: FlatButton(
-                    child: Text(
-                      'Teacher',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    color: Colors.cyan,
-                    textColor: Colors.black,
-                    onPressed: () {},
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(25),
-                  child: FlatButton(
-                    child: Text(
-                      'Student',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    color: Colors.cyan,
-                    textColor: Colors.black,
-                    onPressed: () {},
-                  ),
-                ),
-              ]))),
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
     );
   }
 }
